d0364f8a2f11f050d5c838cf7a846bcb41467a39
jhHan <ardente1990@gmail.com>
2021-04-18 16:20:41 -0500

    Update: Line plot
    - Line plot: matplotlib -> plotly
    -

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import plotly.express as px
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as pt
+import plotly.express as px
 from dash.dependencies import Input, Output
 # import seaborn as sns
 import os
@@ -19,69 +20,58 @@ import os
 # from pandas import ExcelWriter
 # from Basic_cal import BasicStat
 
-df = px.data.iris()
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
-# data_path = r"E:\03_Codes\02_codable_2021\backend\Model_eval\streamflow_andong.csv"
-# data =  pd.read_csv(data_path)
-# data['Date'] = pd.to_datetime(data['Date'])   #Read date
-# date = data['Date']
-# obs = data['obs']
-# sim = data['sim']
-
-# pt.figure(figsize= (6,6))
-# pt.grid()
-# pt.gca().set_aspect("equal")
-# # fig = pt.scatter(obs,sim, c=sim, s=5, vmin=min(obs), vmax=max(obs))
-# fig = px.scatter(x=date, y=obs)
-# pt.colorbar(fig, shrink=0.8)
-# pt.plot(obs,obs,label = '1:1')
-
+colors = {'background': '#111111', 'text': '#7FDBFF'}
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
+# fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+# fig.update_layout(font_color=colors['text'])
+
+
+data_path = r"E:\03_Codes\02_codable_2021\backend\Model_eval\streamflow_andong.csv"
+data =  pd.read_csv(data_path)
+data['Date'] = pd.to_datetime(data['Date'])   #Read date
+date = data['Date']
+obs = data['obs']
+sim = data['sim']
+
+fig = px.line(data, x="Date", y=data.columns,
+              hover_data={"Date": "|%B %d, %Y"},
+              title='custom tick labels')
+fig.update_xaxes(
+    dtick="M6",
+    tickformat="%m\n%Y")
 fig.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
+    xaxis_title = "Date",
+    yaxis_title = "",
+    legend_title = ""
 )
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Model Performance Assessment Tool',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
 
-    html.Div(children='Assess your model outputs by calculating statistic indice and ploting.', style={
-        'textAlign': 'center',
-        'color': colors['text']
+app.layout = html.Div(children=[
+    html.H1(children='Model Evaluation',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }),
+
+    html.Div(children='''
+        Model evaluation metircs calculation tool.
+    ''', style ={'textAlign': 'center', 'color': colors['text']
     }),
 
     dcc.Graph(
-        id='example-graph-2',
+        id='example-graph',
         figure=fig
     )
 ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
