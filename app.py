d8a5183174316bc07ee9c1f7d2c1f2f5d98cbba4
jhHan <ardente1990@gmail.com>
2021-05-02 16:46:50 -0500

    Callback edit

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ app.layout = html.Div(children=[
     # Allow multiple files to be upload
     multiple=True
     ),
-    dcc.Graph(id='line-plot'),
+    # dcc.Graph(id='line-plot'),
     html.Div(id='output-data-upload'),
 ])
     
@@ -73,47 +73,10 @@ def parse_contents(contents, filename, date):
             'There was an error processing this file.'
         ])
 
-    return html.Div([
-        html.H5(filename),
-        html.H6(datetime.datetime.fromtimestamp(date)),
-
-        dash_table.DataTable(
-            data=df.to_dict('records'),
-            columns=[{'name': i, 'id': i} for i in df.columns],
-            page_size=10,
-            style_table={'height': '300px', 'overflowY': 'auto'}
-        ),
-
-        html.Hr()  # horizontal line
-    ])
-
-@app.callback(
-    Output('line-plot', 'figure'),
-    Output('output-data-upload', 'children'),
-    Input('upload-data', 'contents'),
-    State('upload-data', 'filename'),
-    State('upload-data', 'last_modified'))
-def update_figure(contents, names, dates):
-    content_type, content_string = contents.split(',')
-
-    decoded = base64.b64decode(content_string)
-    try:
-        if 'csv' in names:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')))
-        elif 'xls' in names:
-            # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded))
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
     df['Date'] = pd.to_datetime(df['Date'])   #Read date
     date = df['Date']
-    obs = df['obs']
-    sim = df['sim']
+    obs = df['Measurement']
+    sim = df['Simulation']
 
     fig = px.line(df, x="Date", y=df.columns,
                 hover_data={"Date": "|%B %d, %Y"},
@@ -125,13 +88,67 @@ def update_figure(contents, names, dates):
         xaxis_title = "Date",
         yaxis_title = "",
         legend_title = "")
-    return fig
+
+    return html.Div([
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+
+        dash_table.DataTable(
+            data=df.to_dict('records'),
+            columns=[{'name': i, 'id': i} for i in df.columns],
+            page_size=10,
+            style_table={'height': '300px', 'overflowY': 'auto'}
+        ),
+
+        html.Hr(),  # horizontal line
+
+        dcc.Graph(figure=fig)
+    ])
+
+@app.callback(
+    Output('output-data-upload', 'children'),
+    Input('upload-data', 'contents'),
+    State('upload-data', 'filename'),
+    State('upload-data', 'last_modified'))
+# def update_figure(contents, names, dates):
+#     content_type, content_string = contents.split(',')
+
+#     decoded = base64.b64decode(content_string)
+#     try:
+#         if 'csv' in names:
+#             # Assume that the user uploaded a CSV file
+#             df = pd.read_csv(
+#                 io.StringIO(decoded.decode('utf-8')))
+#         elif 'xls' in names:
+#             # Assume that the user uploaded an excel file
+#             df = pd.read_excel(io.BytesIO(decoded))
+#     except Exception as e:
+#         print(e)
+#         return html.Div([
+#             'There was an error processing this file.'
+#         ])
+#     df['Date'] = pd.to_datetime(df['Date'])   #Read date
+#     date = df['Date']
+#     obs = df['obs']
+#     sim = df['sim']
+
+#     fig = px.line(df, x="Date", y=df.columns,
+#                 hover_data={"Date": "|%B %d, %Y"},
+#                 title='Prediction VS Observation')
+#     fig.update_xaxes(
+#         # dtick="M6",
+#         tickformat="%m\n%Y")
+#     fig.update_layout(
+#         xaxis_title = "Date",
+#         yaxis_title = "",
+#         legend_title = "")
+#     return fig
 def update_output(list_of_contents, list_of_names, list_of_dates):
     if list_of_contents is not None:
         children = [
             parse_contents(c, n, d) for c, n, d in
             zip(list_of_contents, list_of_names, list_of_dates)]
-        return children
+        return fig, children
 
 if __name__ == '__main__':
     app.run_server(debug=True)
