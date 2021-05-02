f6a14d3ebb300e9771f57971fc97855fdc35cb06
jhHan <ardente1990@gmail.com>
2021-05-02 16:18:04 -0500

    Callback update
    - output: figure

--- app.py
+++ app.py
@@ -16,45 +16,13 @@ import numpy as np
 import matplotlib.pyplot as pt
 import plotly.express as px
 import dash_table
-
 from dash.dependencies import Input, Output, State
-
-# import seaborn as sns
 import os
-# from tkinter import *
-# from tkinter import filedialog
-# from pandas import ExcelFile
-# from pandas import ExcelWriter
-# from Basic_cal import BasicStat
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 colors = {'background': '#111111', 'text': '#7FDBFF'}
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-
-data_path = r"E:\03_Codes\02_codable_2021\backend\Model_eval\streamflow_andong.csv"
-data =  pd.read_csv(data_path)
-data['Date'] = pd.to_datetime(data['Date'])   #Read date
-date = data['Date']
-obs = data['obs']
-sim = data['sim']
-
-fig = px.line(data, x="Date", y=data.columns,
-              hover_data={"Date": "|%B %d, %Y"},
-              title='Prediction VS Observation')
-fig.update_xaxes(
-    # dtick="M6",
-    tickformat="%m\n%Y")
-fig.update_layout(
-    xaxis_title = "Date",
-    yaxis_title = "",
-    legend_title = ""
-)
-
 
 app.layout = html.Div(children=[
     html.H1(children='Model Evaluation',
@@ -65,15 +33,6 @@ app.layout = html.Div(children=[
     html.Div(children='''
         Model evaluation metircs calculation tool.
     ''', style ={'textAlign': 'center', 'color': colors['text']}),  
-
-    # html.Div(children = ["y-axis title: ",
-    #             dcc.Input(id='my-input', value='initial value', type='text')]),
-    # html.Br(),
-    # html.Div(id='my-output'),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig),
 
     dcc.Upload(
         id='upload-data',
@@ -92,6 +51,7 @@ app.layout = html.Div(children=[
     # Allow multiple files to be upload
     multiple=True
     ),
+    dcc.Graph(id='line-plot'),
     html.Div(id='output-data-upload'),
 ])
     
@@ -122,33 +82,56 @@ def parse_contents(contents, filename, date):
             columns=[{'name': i, 'id': i} for i in df.columns],
             page_size=10,
             style_table={'height': '300px', 'overflowY': 'auto'}
-
         ),
 
-        html.Hr(),  # horizontal line
-
-        # For debugging, display the raw contents provided by the web browser
-        html.Div('Raw Content'),
-        html.Pre(contents[0:20] + '...', style={
-            'whiteSpace': 'pre-wrap',
-            'wordBreak': 'break-all'
-        })
+        html.Hr()  # horizontal line
     ])
 
-@app.callback(Output('output-data-upload', 'children'),
+@app.callback(
+    Output('line-plot', 'figure'),
+    Output('output-data-upload', 'children'),
     Input('upload-data', 'contents'),
     State('upload-data', 'filename'),
     State('upload-data', 'last_modified'))
+def update_figure(contents, names, dates):
+    content_type, content_string = contents.split(',')
 
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in names:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in names:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+    df['Date'] = pd.to_datetime(df['Date'])   #Read date
+    date = df['Date']
+    obs = df['obs']
+    sim = df['sim']
+
+    fig = px.line(df, x="Date", y=df.columns,
+                hover_data={"Date": "|%B %d, %Y"},
+                title='Prediction VS Observation')
+    fig.update_xaxes(
+        # dtick="M6",
+        tickformat="%m\n%Y")
+    fig.update_layout(
+        xaxis_title = "Date",
+        yaxis_title = "",
+        legend_title = "")
+    return fig
 def update_output(list_of_contents, list_of_names, list_of_dates):
     if list_of_contents is not None:
         children = [
             parse_contents(c, n, d) for c, n, d in
             zip(list_of_contents, list_of_names, list_of_dates)]
         return children
-
-def update_output_div(input_value):
-    return 'Output: {}'.format(input_value)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
